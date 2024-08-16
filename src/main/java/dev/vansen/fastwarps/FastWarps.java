c563d31f9d0c591642813b41d2ec45d4fff6e39c
vansencool <vansencool@gmail.com>
2024-08-16 16:59:41 +0530

    Initial commit

---
+++ src/main/java/dev/vansen/fastwarps/FastWarps.java
@@ -0,0 +1,11 @@
+package dev.vansen.fastwarps;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FastWarps extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO
+    }
+}
